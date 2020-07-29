c2948544b8b595c0cdf63282af721114b8a30232
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-30 02:29:21 +0900

    feat(login module): import mat

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
