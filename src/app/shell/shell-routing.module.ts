db9ab38284058355d83b8a1006f60899d8dd27c8
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-07-30 23:28:09 +0900

    feat(shell routing): add AuthGuard

--- src/app/shell/shell-routing.module.ts
+++ src/app/shell/shell-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShellComponent } from './shell/shell.component';
+import { AuthGuard } from '../guards/auth.guard';
 
 
 const routes: Routes = [
   {
     path: '',
     component: ShellComponent,
+    canActivate: [AuthGuard],
+    canLoad: [AuthGuard],
     children: []
   }
 ];
