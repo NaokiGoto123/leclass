0815591b453a031cfece871657c214faf8163787
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-03 02:15:55 +0900

    feat(lesson get) create

--- src/app/services/lesson-get.service.ts
+++ src/app/services/lesson-get.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Lesson } from '../interfaces/lesson';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +14,41 @@ export class LessonGetService {
   ) { }
 
   getLesson(id: string): Observable<Lesson> {
-    return this.db.doc<Lesson>(`lessons/${id}`).valueChanges();
+    return this.db.doc<Lesson>(`lessons/${id}`).valueChanges()
+    .pipe(
+      map((lesson: Lesson) => {
+        if (lesson) {
+          return lesson;
+        } else {
+          return null;
+        }
+      })
+    );
   }
 
   getLessons(): Observable<Lesson[]> {
-    return this.db.collection<Lesson>(`lessons`).valueChanges();
+    return this.db.collection<Lesson>(`lessons`).valueChanges()
+    .pipe(
+      map((lessons: Lesson[]) => {
+        if (lessons.length) {
+          return lessons;
+        } else {
+          return [];
+        }
+      })
+    );;
+  }
+
+  getSpecificLessons(subject: string): Observable<Lesson[]> {
+    return this.db.collection<Lesson>(`lessons`, (ref) => ref.where('subject', '==', subject)).valueChanges()
+    .pipe(
+      map((lessons: Lesson[]) => {
+        if (lessons.length) {
+          return lessons;
+        } else {
+          return [];
+        }
+      })
+    );
   }
 }
