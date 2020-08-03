a52d467d6929d51730848dfa327c913aa3cfb781
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-03 12:25:34 +0900

    feat(purpose): create

--- src/app/shell/about/about.module.ts
+++ src/app/shell/about/about.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about/about.component';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { PurposeComponent } from './purpose/purpose.component';
+import { UsageComponent } from './usage/usage.component';
 
 @NgModule({
-  declarations: [AboutComponent],
+  declarations: [AboutComponent, PurposeComponent, UsageComponent],
   imports: [
     CommonModule,
-    AboutRoutingModule
+    AboutRoutingModule,
+    MatTabsModule
   ]
 })
 export class AboutModule { }
