3a7866d9cc6d23768d3e1afba5c51074bd626dea
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-03 18:29:40 +0900

    feat(lesson): add dom sanitizer

--- src/app/shell/lesson/lesson/lesson.component.ts
+++ src/app/shell/lesson/lesson/lesson.component.ts
@@ -5,6 +5,7 @@ import { Lesson } from 'src/app/interfaces/lesson';
 import { Location } from '@angular/common';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'firebase';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-lesson',
@@ -15,18 +16,22 @@ export class LessonComponent implements OnInit {
 
   lesson: Lesson;
 
+  safeHtml: SafeHtml;
+
   creater: User;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private location: Location,
     private lessonGetService: LessonGetService,
-    private userService: UserService
+    private userService: UserService,
+    private domSanitizer: DomSanitizer
   ) {
     this.activatedRoute.queryParamMap.subscribe((params) => {
       const id = params.get('id');
       this.lessonGetService.getLesson(id).subscribe((lesson: Lesson) => {
         this.lesson = lesson;
+        this.safeHtml = this.domSanitizer.bypassSecurityTrustHtml(lesson.content);
         this.userService.getUser(lesson.createrId).subscribe((creater: User) => {
           this.creater = creater;
         });
