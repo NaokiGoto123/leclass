2c91f71e487ff1e064f775f525e449e3453c72f3
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-05 00:35:12 +0900

    feat(enter lesson editor guard): create

--- src/app/shell/create-lesson/create-lesson-routing.module.ts
+++ src/app/shell/create-lesson/create-lesson-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CreateLessonComponent } from './create-lesson/create-lesson.component';
 import { LessonEditorGuard } from 'src/app/guards/lesson-editor.guard';
+import { EnterLessonEditorGuard } from 'src/app/guards/enter-lesson-editor.guard';
 
 
 const routes: Routes = [
   {
     path: '',
     component: CreateLessonComponent,
-    canDeactivate: [LessonEditorGuard]
+    canDeactivate: [LessonEditorGuard],
+    canActivate: [EnterLessonEditorGuard],
+    canLoad: [EnterLessonEditorGuard]
   }
 ];
 
