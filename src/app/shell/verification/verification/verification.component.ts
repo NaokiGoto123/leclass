af4978628728d0ffca71c4b5864615e6fd70bc5f
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-04 22:14:14 +0900

    feat(verification): add buttons

--- src/app/shell/verification/verification/verification.component.ts
+++ src/app/shell/verification/verification/verification.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { VerificationService } from 'src/app/services/verification.service';
 import { User } from 'src/app/interfaces/user';
 import { VerificationGetService } from 'src/app/services/verification-get.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-verification',
@@ -10,18 +11,31 @@ import { VerificationGetService } from 'src/app/services/verification-get.servic
 })
 export class VerificationComponent implements OnInit {
 
+  user: User;
+
   verifiedUsers: User[];
+
+  verificationRequests: string[];
 
   constructor(
     private verificationService: VerificationService,
-    private verificationGetService: VerificationGetService
+    private verificationGetService: VerificationGetService,
+    private authService: AuthService
   ) {
+    this.user = this.authService.user;
     this.verificationGetService.getVerifiedUsers().subscribe((verifiedUsers: User[]) => {
       this.verifiedUsers = verifiedUsers;
+    });
+    this.verificationGetService.getVerificationRequests().subscribe((requests: string[]) => {
+      this.verificationRequests = requests;
     });
   }
 
   ngOnInit(): void {
+  }
+
+  sendVerificationRequest() {
+    this.verificationService.sendVerificationRequests(this.authService.user.uid);
   }
 
 }
