cf4af3b85d349322a73de7044d1897d42c6d2b33
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-04 13:08:49 +0900

    feat(login routing): disable login gurad

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -8,8 +8,8 @@ const routes: Routes = [
   {
     path: '',
     component: LoginComponent,
-    canActivate: [LoginGuard],
-    canLoad: [LoginGuard]
+    // canActivate: [LoginGuard],
+    // canLoad: [LoginGuard]
   }
 ];
 
