8c0f7dcb79e9222368b36dd7114a976d0fda8dfb
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-13 16:47:01 +0900

    feat(home): add search()

--- src/app/shell/home/home/home.component.ts
+++ src/app/shell/home/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LessonGetService } from 'src/app/services/lesson-get.service';
 import { Lesson } from 'src/app/interfaces/lesson';
+import { SearchService } from 'src/app/services/search.service';
+import { SearchIndex } from 'algoliasearch/lite';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +12,23 @@ import { Lesson } from 'src/app/interfaces/lesson';
 })
 export class HomeComponent implements OnInit {
 
+  private index: SearchIndex = this.searchService.index.lessons;
+
   lessons: Lesson[];
 
   initialLoading: boolean;
 
+  result: {
+    nbHits: number;
+    hits: any[];
+  };
+
+  query: string;
+
   constructor(
-    private lessonGetService: LessonGetService
+    private lessonGetService: LessonGetService,
+    private searchService: SearchService,
+    private activatedRoute: ActivatedRoute
   ) {
     this.initialLoading = true;
     this.lessonGetService.getLessons().subscribe((lessons: Lesson[]) => {
@@ -23,9 +37,25 @@ export class HomeComponent implements OnInit {
         this.initialLoading = false;
       }, 500);
     });
+    this.activatedRoute.queryParamMap.subscribe((params) => {
+      this.query = params.get('searchQuery') || '';
+      this.search();
+    });
   }
 
   ngOnInit(): void {
   }
 
+  private search() {
+    this.initialLoading = true;
+    this.index
+      .search(this.query, {
+        facetFilters: `isPublic:true`
+      })
+      .then((result) => {
+        console.log(result);
+        this.result = result;
+        this.initialLoading = false;
+      });
+  }
 }
