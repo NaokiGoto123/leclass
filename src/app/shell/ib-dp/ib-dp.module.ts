89cbecd79af85d2e20794bcf69612b2335e640a6
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-08-24 12:16:03 +0900

    feat(ib dp): add lessons

--- src/app/shell/ib-dp/ib-dp.module.ts
+++ src/app/shell/ib-dp/ib-dp.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { IbDpRoutingModule } from './ib-dp-routing.module';
 import { IbDpComponent } from './ib-dp/ib-dp.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 @NgModule({
   declarations: [IbDpComponent],
   imports: [
     CommonModule,
-    IbDpRoutingModule
+    IbDpRoutingModule,
+    SharedModule,
+    MatProgressSpinnerModule
   ]
 })
 export class IbDpModule { }
--- src/app/shell/ib-dp/ib-dp/ib-dp.component.ts
+++ src/app/shell/ib-dp/ib-dp/ib-dp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Lesson } from 'src/app/interfaces/lesson';
+import { LessonGetService } from 'src/app/services/lesson-get.service';
 
 @Component({
   selector: 'app-ib-dp',
@@ -7,7 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IbDpComponent implements OnInit {
 
-  constructor() { }
+  lessons: Observable<Lesson[]>;
+
+  initialLoading: boolean;
+
+  constructor(
+    private lessonGetService: LessonGetService
+  ) {
+    this.initialLoading = true;
+    this.lessons = this.lessonGetService.getSpecificLessons('Japanese SL');
+    setTimeout(() => {
+      this.initialLoading = false;
+    }, 500);
+  }
 
   ngOnInit(): void {
   }
