7809070b723dfcdc38340bff86832ced1667a651
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-09-14 18:45:18 +0900

    feat(supporters): add meta

--- src/app/shell/about/supporters/supporters.component.ts
+++ src/app/shell/about/supporters/supporters.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { VerificationGetService } from 'src/app/services/verification-get.service';
 import { User } from 'src/app/interfaces/user';
 import { take } from 'rxjs/operators';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-supporters',
@@ -13,8 +14,20 @@ export class SupportersComponent implements OnInit {
   verifiedUsers: User[];
 
   constructor(
-    private verificationGetService: VerificationGetService
+    private verificationGetService: VerificationGetService,
+    private titleService: Title,
+    private meta: Meta
   ) {
+    this.titleService.setTitle('Leclass');
+
+    this.meta.addTags([
+      { name: 'description', content: 'Get to know about the supporters' },
+      { property: 'og:title', content: 'Supporters' },
+      { property: 'og:description', content: 'Get to know about the supporters'},
+      { property: 'og:url', content: location.href },
+      { property: 'og:image', content: 'https://leclass-prod.web.app/assets/images/leclass.jpg' }
+    ]);
+
     this.verificationGetService.getVerifiedUsers().pipe(take(1)).subscribe((verifiedUsers: User[]) => {
       this.verifiedUsers = verifiedUsers;
     });
