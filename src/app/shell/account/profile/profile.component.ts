d5bc3752ce70d87fa2743f4cda5c7567f41b556f
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-09-14 18:45:51 +0900

    feat(profile): add meta

--- src/app/shell/account/profile/profile.component.ts
+++ src/app/shell/account/profile/profile.component.ts
@@ -3,6 +3,7 @@ import { UserService } from 'src/app/services/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { switchMap, take } from 'rxjs/operators';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-profile',
@@ -16,7 +17,19 @@ export class ProfileComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private userService: UserService,
+    private titleService: Title,
+    private meta: Meta
   ) {
+    this.titleService.setTitle('Leclass');
+
+    this.meta.addTags([
+      { name: 'description', content: 'Profile' },
+      { property: 'og:title', content: 'Profile' },
+      { property: 'og:description', content: 'Profile'},
+      { property: 'og:url', content: location.href },
+      { property: 'og:image', content: 'https://leclass-prod.web.app/assets/images/leclass.jpg' }
+    ]);
+
     this.activatedRoute.queryParamMap.pipe(
       take(1),
       switchMap((params) => {
