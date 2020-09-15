cc675a538353a9c5e118916de9bb6a455f5769e7
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-09-15 15:54:41 +0900

    feat(login): change favicon

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/interfaces/user';
 import { tap, map, take } from 'rxjs/operators';
+import { DOCUMENT } from '@angular/common';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -18,8 +20,15 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    @Inject(DOCUMENT) private rootDocument: HTMLDocument
   ) {
+    if (!environment.production) {
+      this.rootDocument
+        .querySelector('[rel=icon]')
+        .setAttribute('href', 'favicon-hoge.svg');
+    }
+
     this.initialLoading = true;
   }
 
