1ab514fc4348b012cfaa194345481add198b1cd0
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-09-30 03:27:04 +0900

    feat(reports): add reports guard

---
+++ src/app/guards/reports.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Location } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportsGuard implements CanActivate, CanLoad {
+  constructor(private authService: AuthService, private location: Location) { }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.authService.user.verified) {
+      this.location.back();
+      return;
+    }
+    return true;
+  }
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.user.verified) {
+      this.location.back();
+      return;
+    }
+    return true;
+  }
+}
--- src/app/shell/reports/reports-routing.module.ts
+++ src/app/shell/reports/reports-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReportsGuard } from 'src/app/guards/reports.guard';
 import { ReportsComponent } from './reports/reports.component';
 
 
 const routes: Routes = [
   {
     path: '',
-    component: ReportsComponent
+    component: ReportsComponent,
+    canActivate: [ReportsGuard],
+    canLoad: [ReportsGuard]
   }
 ];
 
