5e7d613df366b62cf0925d8659d940c1ec826134
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-09-29 18:43:49 +0900

    feat(drafts): use unsubscribe() instead of take(1)

--- src/app/shell/drafts/drafts/drafts.component.ts
+++ src/app/shell/drafts/drafts/drafts.component.ts
@@ -1,18 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { LessonGetService } from 'src/app/services/lesson-get.service';
 import { Lesson } from 'src/app/interfaces/lesson';
-import { take } from 'rxjs/operators';
 import { Title, Meta } from '@angular/platform-browser';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-drafts',
   templateUrl: './drafts.component.html',
   styleUrls: ['./drafts.component.scss']
 })
-export class DraftsComponent implements OnInit {
+export class DraftsComponent implements OnInit, OnDestroy {
 
   drafts: Lesson[];
+
+  subscription: Subscription;
 
   constructor(
     private lessonGetService: LessonGetService,
@@ -30,12 +32,16 @@ export class DraftsComponent implements OnInit {
       { property: 'og:image', content: 'https://leclass-prod.web.app/assets/images/leclass.jpg' }
     ]);
 
-    this.lessonGetService.getUnpublishedLessons(this.authService.user.uid).pipe(take(1)).subscribe((drafts: Lesson[]) => {
+    this.subscription = this.lessonGetService.getUnpublishedLessons(this.authService.user.uid).subscribe((drafts: Lesson[]) => {
       this.drafts = drafts;
     });
   }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
