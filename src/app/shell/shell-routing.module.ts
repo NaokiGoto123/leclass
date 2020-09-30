a6b8b8841a095f3d08d34826a12157ad16a59c2f
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-09-30 15:16:52 +0900

    feat(unkown): delete

--- src/app/shell/shell-routing.module.ts
+++ src/app/shell/shell-routing.module.ts
@@ -66,10 +66,6 @@ const routes: Routes = [
       {
         path: 'profile-settings',
         loadChildren: () => import('./profile-settings/profile-settings.module').then((m) => m.ProfileSettingsModule)
-      },
-      {
-        path: '**',
-        loadChildren: () => import('./unknown/unknown.module').then((m) => m.UnknownModule)
       }
     ]
   }
