dbeec95c89d2b5fad8559e3145fbb20c7b757b8c
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-10-01 02:58:20 +0900

    feat(guards): add return false

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,36 +1,60 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import {
+  CanActivate,
+  CanLoad,
+  Route,
+  UrlSegment,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  UrlTree,
+  Router,
+} from '@angular/router';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map, take, tap } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginGuard implements CanActivate, CanLoad {
-  constructor(private afAuth: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.afAuth.user$.pipe(
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.user$.pipe(
       map((user) => !!user),
       tap((isLoggedin) => {
         if (isLoggedin) {
+          console.log('1');
           this.router.navigateByUrl('/');
+          return false;
         }
+        console.log('2');
+        return true;
       })
     );
   }
   canLoad(
     route: Route,
-    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return this.afAuth.user$.pipe(
+    segments: UrlSegment[]
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    return this.authService.user$.pipe(
       map((user) => !!user),
+      take(1),
       tap((isLoggedin) => {
         if (isLoggedin) {
+          console.log('3');
           this.router.navigateByUrl('/');
+          return false;
         }
+        console.log('4');
+        return true;
       })
     );
   }
