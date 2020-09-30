b8dd5214949dd23ecc881233815a9acf24108458
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-10-01 02:58:29 +0900

    feat(app): remove import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { NotfoundModule } from './notfound/notfound.module';
-
 
 const routes: Routes = [
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then((m) => m.LoginModule)
+    loadChildren: () =>
+      import('./login/login.module').then((m) => m.LoginModule),
   },
   {
     path: '',
-    loadChildren: () => import('./shell/shell.module').then((m) => m.ShellModule)
+    loadChildren: () =>
+      import('./shell/shell.module').then((m) => m.ShellModule),
   },
   {
     path: '404',
-    loadChildren: () => import('./notfound/notfound.module').then((m) => m.NotfoundModule)
+    loadChildren: () =>
+      import('./notfound/notfound.module').then((m) => m.NotfoundModule),
   },
   {
     path: '**',
-    redirectTo: '404'
-  }
+    redirectTo: '404',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
