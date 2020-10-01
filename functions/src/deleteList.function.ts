093aaa5ee5b057a07f2e758adef70bbb0daaa19c
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-10-01 16:21:06 +0900

    feat(functions): remove 2GB

--- functions/src/deleteList.function.ts
+++ functions/src/deleteList.function.ts
@@ -2,21 +2,18 @@ import * as functions from 'firebase-functions';
 
 const firebase_tools = require('firebase-tools');
 
-export const deleteList =
-functions
-.runWith({
-  timeoutSeconds: 540,
-  memory: '2GB',
-})
-.region('asia-northeast1')
-.https.onCall(async (uid, context) => {
+export const deleteList = functions
+  .runWith({
+    timeoutSeconds: 540,
+  })
+  .region('asia-northeast1')
+  .https.onCall(async (uid, context) => {
+    const pathToLesson = `users/${uid}/list`;
 
-  const pathToLesson = `users/${uid}/list`
-
-  await firebase_tools.firestore.delete(pathToLesson, {
-    project: process.env.GCLOUD_PROJECT,
-    recursive: true,
-    yes: true,
-    token: functions.config().fb.token,
+    await firebase_tools.firestore.delete(pathToLesson, {
+      project: process.env.GCLOUD_PROJECT,
+      recursive: true,
+      yes: true,
+      token: functions.config().fb.token,
+    });
   });
-})
