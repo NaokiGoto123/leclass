59c33684b4e8771a5686f17730d456dec3746c0b
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-11-19 21:33:59 +0900

    feat(archived courses): update

--- src/app/shell/archived-courses/archived-courses.module.ts
+++ src/app/shell/archived-courses/archived-courses.module.ts
@@ -3,13 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { ArchivedCoursesRoutingModule } from './archived-courses-routing.module';
 import { ArchivedCoursesComponent } from './archived-courses/archived-courses.component';
-
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [ArchivedCoursesComponent],
   imports: [
     CommonModule,
-    ArchivedCoursesRoutingModule
+    ArchivedCoursesRoutingModule,
+    MatListModule,
+    MatDividerModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatTooltipModule
   ]
 })
 export class ArchivedCoursesModule { }
--- src/app/shell/archived-courses/archived-courses/archived-courses.component.ts
+++ src/app/shell/archived-courses/archived-courses/archived-courses.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+import { Observable, pipe } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Subject } from 'src/app/interfaces/subject';
+import { SubjectService } from 'src/app/services/subject.service';
 
 @Component({
   selector: 'app-archived-courses',
@@ -7,7 +12,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArchivedCoursesComponent implements OnInit {
 
-  constructor() { }
+  archivedCourses: Subject[] = [];
+
+  initialLoading: boolean;
+
+  constructor(
+    private subjectService: SubjectService,
+    private titleService: Title,
+    private meta: Meta
+  ) {
+    this.titleService.setTitle('Archived courses | Leclass');
+
+    this.meta.addTags([
+      { name: 'description', content: 'Archived courses' },
+      { property: 'og:title', content: 'Archived courses' },
+      { property: 'og:description', content: 'Archived courses' },
+      { property: 'og:url', content: location.href },
+      {
+        property: 'og:image',
+        content: 'https://leclass-prod.web.app/assets/images/leclass.jpg',
+      },
+    ]);
+
+    this.subjectService.getSubjects().pipe(
+    take(1))
+    .subscribe((subjects: Subject[]) => {
+      subjects.map((subject: Subject) => {
+        console.log(subject.name);
+        if (subject.archived) {
+          this.archivedCourses.push(subject);
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
   }
