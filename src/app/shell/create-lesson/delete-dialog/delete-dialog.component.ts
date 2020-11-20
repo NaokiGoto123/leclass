28b2248e83be93b53d5bf0350fb4d506c99e9313
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-11-20 15:46:30 +0900

    feat(delete dialog): remove videoId

--- src/app/shell/create-lesson/delete-dialog/delete-dialog.component.ts
+++ src/app/shell/create-lesson/delete-dialog/delete-dialog.component.ts
@@ -6,30 +6,25 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-delete-dialog',
   templateUrl: './delete-dialog.component.html',
-  styleUrls: ['./delete-dialog.component.scss']
+  styleUrls: ['./delete-dialog.component.scss'],
 })
 export class DeleteDialogComponent implements OnInit {
-
   id: string;
-
-  videoId: string;
 
   constructor(
     private lessonService: LessonService,
     private router: Router,
     public dialogRef: MatDialogRef<DeleteDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: {id: string, videoId: string}
+    @Inject(MAT_DIALOG_DATA) public data: { id: string }
   ) {
     this.id = data.id;
-    this.videoId = data.videoId;
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   deleteLesson() {
     this.dialogRef.close();
-    this.lessonService.deleteLesson(this.id, this.videoId);
+    this.lessonService.deleteLesson(this.id);
     this.router.navigateByUrl('/');
   }
 }
