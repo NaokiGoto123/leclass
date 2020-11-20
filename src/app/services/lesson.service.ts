73cc92e5dcc9374601bb7ba4be4af516ad1245c7
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-11-20 15:45:22 +0900

    feat(lesson service): remove vimeo

--- src/app/services/lesson.service.ts
+++ src/app/services/lesson.service.ts
@@ -3,23 +3,20 @@ import { AngularFirestore } from '@angular/fire/firestore';
 import { Lesson } from '../interfaces/lesson';
 import { AngularFireFunctions } from '@angular/fire/functions';
 import { firestore } from 'firebase';
-import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LessonService {
-
   constructor(
     private db: AngularFirestore,
-    private fns: AngularFireFunctions,
-    private http: HttpClient
-  ) { }
+    private fns: AngularFireFunctions
+  ) {}
 
   createLesson(lesson: Omit<Lesson, 'date'>) {
     this.db.doc(`lessons/${lesson.id}`).set({
       ...lesson,
-      date: firestore.Timestamp.now()
+      date: firestore.Timestamp.now(),
     });
   }
 
@@ -27,13 +24,8 @@ export class LessonService {
     this.db.doc<Lesson>(`lessons/${lesson.id}`).update(lesson);
   }
 
-  async deleteLesson(id: string, videoId: string) {
+  async deleteLesson(id: string) {
     const deleteLesson = this.fns.httpsCallable('deleteLesson');
     const result = await deleteLesson(id).toPromise();
-    const vimeoDeletion = await this.http.delete(`https://api.vimeo.com/videos/${videoId}`, {
-      headers: new HttpHeaders({
-        Authorization: `bearer 131de8827dca0fa95e1fadae192e3bf7`,
-      }),
-    }).toPromise();
   }
 }
