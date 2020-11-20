b4808629a4134a01f694524a3fa868a42d419c88
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-11-20 21:20:39 +0900

    feat(lesson get): update method

--- src/app/services/lesson-get.service.ts
+++ src/app/services/lesson-get.service.ts
@@ -5,64 +5,75 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LessonGetService {
-
-  constructor(
-    private db: AngularFirestore
-  ) { }
+  constructor(private db: AngularFirestore) {}
 
   getLesson(id: string): Observable<Lesson> {
-    return this.db.doc<Lesson>(`lessons/${id}`).valueChanges()
-    .pipe(
-      map((lesson: Lesson) => {
-        if (lesson) {
-          return lesson;
-        } else {
-          return null;
-        }
-      })
-    );
+    return this.db
+      .doc<Lesson>(`lessons/${id}`)
+      .valueChanges()
+      .pipe(
+        map((lesson: Lesson) => {
+          if (lesson) {
+            return lesson;
+          } else {
+            return null;
+          }
+        })
+      );
   }
 
   getLessons(): Observable<Lesson[]> {
-    return this.db.collection<Lesson>(`lessons`, (ref) => ref.where('isPublic', '==', true)).valueChanges()
-    .pipe(
-      map((lessons: Lesson[]) => {
-        if (lessons.length) {
-          return lessons;
-        } else {
-          return [];
-        }
-      })
-    );
+    return this.db
+      .collection<Lesson>(`lessons`, (ref) => ref.where('isPublic', '==', true))
+      .valueChanges()
+      .pipe(
+        map((lessons: Lesson[]) => {
+          if (lessons.length) {
+            return lessons;
+          } else {
+            return [];
+          }
+        })
+      );
   }
 
   getSpecificLessons(subjectId: string): Observable<Lesson[]> {
-    return this.db.collection<Lesson>(`lessons`, (ref) => ref.where('subjectId', '==', subjectId).where('isPublic', '==', true))
-    .valueChanges()
-    .pipe(
-      map((lessons: Lesson[]) => {
-        if (lessons.length) {
-          return lessons;
-        } else {
-          return [];
-        }
-      })
-    );
+    return this.db
+      .collection<Lesson>(`lessons`, (ref) =>
+        ref
+          .where('subjectId', '==', subjectId)
+          .where('isPublic', '==', true)
+          .orderBy('number')
+      )
+      .valueChanges()
+      .pipe(
+        map((lessons: Lesson[]) => {
+          if (lessons.length) {
+            return lessons;
+          } else {
+            return [];
+          }
+        })
+      );
   }
 
   getUnpublishedLessons(uid: string): Observable<Lesson[]> {
-    return this.db.collection(`lessons`, (ref) => ref.where('isPublic', '==', false).where('createrId', '==', uid)).valueChanges()
-    .pipe(
-      map((unpublishedLessons: Lesson[]) => {
-        if (unpublishedLessons.length) {
-          return unpublishedLessons;
-        } else {
-          return [];
-        }
-      })
-    );
+    return this.db
+      .collection(`lessons`, (ref) =>
+        ref.where('isPublic', '==', false).where('createrId', '==', uid)
+      )
+      .valueChanges()
+      .pipe(
+        map((unpublishedLessons: Lesson[]) => {
+          if (unpublishedLessons.length) {
+            return unpublishedLessons;
+          } else {
+            return [];
+          }
+        })
+      );
   }
 }
