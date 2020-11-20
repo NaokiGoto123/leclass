a573c4488ac5fa3d7fb6a27768f351cad8610893
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-11-20 15:48:09 +0900

    feat(environment): remove vimeo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,13 +8,10 @@ export const environment = {
     storageBucket: 'leclass-3ef28.appspot.com',
     messagingSenderId: '113948772810',
     appId: '1:113948772810:web:1e47c2d4acfc3ed085e733',
-    measurementId: 'G-FSSRL67P5H'
+    measurementId: 'G-FSSRL67P5H',
   },
   algolia: {
     appId: '5XXQXHCD4W',
     searchKey: '8d8dd1c99c197cc850465ff019a59530',
   },
-  vimeo: {
-    token: '131de8827dca0fa95e1fadae192e3bf7'
-  }
 };
