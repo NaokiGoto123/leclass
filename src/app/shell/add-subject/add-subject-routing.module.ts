4566ea0903377622106bea84333acb782f64ada3
NaokiGoto123 <baron.naoki.g@gmail.com>
2020-11-25 03:21:49 +0900

    feat(add subject): add guard

--- src/app/shell/add-subject/add-subject-routing.module.ts
+++ src/app/shell/add-subject/add-subject-routing.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddSubjectGuard } from 'src/app/guards/add-subject.guard';
 import { AddSubjectComponent } from './add-subject/add-subject.component';
-
 
 const routes: Routes = [
   {
     path: '',
-    component: AddSubjectComponent
-  }
+    component: AddSubjectComponent,
+    canActivate: [AddSubjectGuard],
+    canLoad: [AddSubjectGuard],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AddSubjectRoutingModule { }
+export class AddSubjectRoutingModule {}
